eedfc56f1490bdfd4f071834c0d784a61983d2c9
balyam <balyam@mail.ru>
2021-06-15 15:14:21 +0600

    refactor viewset for parameters

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'branches', views.BranchViewSet)
+router.register(r'branches', views.BranchViewSet, basename='branches')
 
 
 urlpatterns = [
--- main/views.py
+++ main/views.py
@@ -2,10 +2,19 @@ from .models import BranchModel
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import BranchSerializer
+from django.db.models import Q
 
 # Create your views here.
 
 class BranchViewSet(viewsets.ModelViewSet):
-    queryset = BranchModel.objects.all().order_by('city_ru')
+    #queryset = BranchModel.objects.all().order_by('city_ru')
     serializer_class = BranchSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = BranchModel.objects.all()
+        city = self.request.query_params.get('city')
+        alias = self.request.query_params.get('branch')
+        if city is not None:
+            queryset = queryset.filter(city_ru__iexact=city).filter(address_ru__icontains=alias)
+        return queryset
